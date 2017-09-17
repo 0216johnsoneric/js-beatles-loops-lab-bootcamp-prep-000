a4f64e0a57e4f3e4cc9a315a7001ff074947a07f
Eric Johnson <0216johnsoneric@gmail.com>
2017-09-17 05:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+// var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
   for(var i =0; i < musicians.length; i++) {
@@ -6,12 +8,12 @@ function theBeatlesPlay(musicians, instruments) {
   return newArray;
 }
 
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+// var facts = [
+//   "He was the last Beatle to learn to drive",
+//   "He was never a vegetarian",
+//   "He was a choir boy and boy scout",
+//   "He hated the sound of his own voice"
+// ];
 
 function johnLennonFacts(array) {
   var result= [];
