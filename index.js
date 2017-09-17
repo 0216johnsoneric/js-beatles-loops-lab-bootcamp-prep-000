a8ca9790079d6b3454290f878d9e2a0898c039fe
Eric Johnson <0216johnsoneric@gmail.com>
2017-09-17 05:35:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(beatles, instruments) {
-  var n = new array();
+  var n = array();
   for (i = 0; i < beatles.length; i++) {
     n.push(`${beatles[i]} plays ${instruments[i]}`);
   }
